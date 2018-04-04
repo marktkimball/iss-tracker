9c3328398c3e10740f456a82ffa8643b3070349d
marktkimball <mark.kimball@octanner.com>
2018-04-03 18:52:38 -0600

    Map style option settings

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -1,11 +1,22 @@
 import React from 'React';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import {
+  Button,
+  Caption,
   Colors as COLORS,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Paragraph,
+  RadioButton,
+  RadioButtonGroup,
+  Subheading,
   Toolbar,
   ToolbarAction,
   ToolbarContent,
+  TouchableRipple,
 } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
@@ -16,9 +27,16 @@ export class Settings extends React.Component {
     history: PropTypes.object,
   };
 
+  state = {
+    showMapThemeDialog: false,
+  };
+
+  toggleMapThemeDialog = () =>
+    this.setState({ showMapThemeDialog: !this.state.showMapThemeDialog });
+
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Toolbar>
           <ToolbarAction
             icon={
@@ -32,7 +50,59 @@ export class Settings extends React.Component {
           />
           <ToolbarContent title="Settings" />
         </Toolbar>
+        <TouchableRipple onPress={this.toggleMapThemeDialog}>
+          <View style={styles.option}>
+            <Subheading>Map Theme</Subheading>
+            <Caption>Dark</Caption>
+          </View>
+        </TouchableRipple>
+        <Dialog
+          dismissable
+          onDismiss={this.toggleMapThemeDialog}
+          visible={this.state.showMapThemeDialog}
+        >
+          <DialogTitle>Map Theme</DialogTitle>
+          <DialogContent>
+            <RadioButtonGroup
+              onValueChange={value => console.warn('VALUE', value)}
+              value={'dark'}
+            >
+              <View style={styles.radioOption}>
+                <RadioButton value="dark" />
+                <Paragraph style={styles.radioButtonText}>Dark</Paragraph>
+              </View>
+              <View style={styles.radioOption}>
+                <RadioButton value="light" />
+                <Paragraph style={styles.radioButtonText}>Light</Paragraph>
+              </View>
+            </RadioButtonGroup>
+          </DialogContent>
+          <DialogActions>
+            <Button onPress={this.toggleMapThemeDialog}>Done</Button>
+          </DialogActions>
+        </Dialog>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.grey50,
+    height: '100%',
+    width: '100%',
+  },
+  option: {
+    borderBottomWidth: 1,
+    borderColor: COLORS.grey200,
+    paddingHorizontal: 20,
+    paddingVertical: 8,
+  },
+  radioOption: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  radioButtonText: {
+    marginLeft: 12,
+  },
+});
